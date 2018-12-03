791d45980275f70485a54d865bb0c804e9ba94cb
Kevin Kragenbrink <kevin.kragenbrink@crowdstrike.com>
2018-12-03 15:38:26 -0800

    beginning hooks for web

--- src/main.js
+++ src/main.js
@@ -19,11 +19,13 @@
 // SOFTWARE.w
 
 import Vue from "vue";
+import Resource from "vue-resource"
 import App from "./App.vue";
 import router from "./router";
 
 Vue.config.productionTip = false;
 
+Vue.use(Resource);
 new Vue({
   router,
   render: h => h(App)
